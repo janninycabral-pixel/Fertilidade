93eac51c39e4f50d015c4181cfafae8e0ccdcf3d
janninycabral-pixel <janninycabral@gmail.com>
2026-06-19 16:59:26 -0300

    Update fertilidade.py

--- fertilidade.py
+++ fertilidade.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
-import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
 import requests
